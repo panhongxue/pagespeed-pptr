af9ec681582deea2d10d5aabeb2d1bc5db1088e4
panhongxue <panliu888@gmail.com>
2022-05-31 19:40:43 +0800

    puppeteer

--- bin/pagespeed.js
+++ bin/pagespeed.js
@@ -1,3 +1,24 @@
 #!/usr/bin/env node
 
-console.log("hello world")
+const program = require('commander')
+const package = require('../package')
+
+program.name('pagespeed').version(`${package.name} ${package.version}`).usage('<url> [options]')
+
+program
+  .arguments('<url>')
+  .description(
+    `网页性能分析工具`
+  )
+  .option('-s, --show-browser', '显示浏览器窗口')
+  .option('-c, --case', '检测营销案例')
+  .action((url, options) => {
+    require('../lib/pptr-tester')(url, options)
+  })
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp()
+  process.exit(0)
+}
+
+program.parse(process.argv)
